c4742d5e403ec15e4ab64999fd43ed4f736347db
Murad Teymurov <murad.teymurov@sinam.net>
2023-12-24 05:04:17 +0400

    consumer retry kafka

---
+++ kafka-retry-job/src/main/java/com/company/kafkaretryjob/KafkaRetryJobApplication.java
@@ -0,0 +1,13 @@
+package com.company.kafkaretryjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRetryJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRetryJobApplication.class, args);
+	}
+
+}
---
+++ kafka-retry-job/src/main/java/com/company/kafkaretryjob/model/OrderEvent.java
@@ -0,0 +1,19 @@
+package com.company.kafkaretryjob.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String id;
+    private String username;
+    private String price;
+    private String createdTime;
+}
+
